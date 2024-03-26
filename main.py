d7842c4b91ac2b4a717824706e1a96c22d8b5e9d
Marie-Laura11 <mldro3@ulaval.ca>
2024-03-25 21:26:48 -0400

    joueurs

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ Module principal
 import quixo
 import api
 
+def main():
+    """
+    Fonction qui fait démarrer les parties
+    """
+    idul_du_joueur = input("Entrez l'IDUL du joueur : ")
+    secret = input("Entrez le jeton personnel du joueur : ")
