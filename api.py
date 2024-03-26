fcf7e12145b77ecf35d70b831ecc4007f466624f
Marie-Laura11 <mldro3@ulaval.ca>
2024-03-25 21:08:10 -0400

    premier

--- api.py
+++ api.py
@@ -1,3 +1,11 @@
-import request 
+"""Module api"""
 
-def list_
+import requests
+
+def lister_parties(idul, secret):
+    """
+    Fonction qui accepte en entrée un nom d'usager (idul) et le jeton 
+    qui lui est associé (secret), et qui retourne en sortie la liste des 
+    parties reçues du serveur, après avoir décodé le JSON de sa réponse.
+    """
+    base_url = 'https://pax.ulaval.ca/quixo/api/h24/'
